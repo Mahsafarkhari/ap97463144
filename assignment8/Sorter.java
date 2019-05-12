35c93213c3a23aacd18808fe2fc6bc61431f8cd9
Mahsafarkhari <mahisa.farkhari@gmail.com>
2019-05-13 03:04:30 +0430

    first commit

---
+++ assignment8/Sorter.java
@@ -0,0 +1,15 @@
+
+package assignment8;
+
+import java.util.Comparator;
+
+public class Sorter {
+    
+
+    public int compare(Student_G o1, Student_G o2) {
+
+        return o1.Number.compareTo(o2.Number);
+
+    }
+    
+}
---
+++ assignment8/main.java
@@ -0,0 +1,26 @@
+
+package assignment8;
+
+import java.io.FileNotFoundException;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+
+public class main {
+    public static void main(String[] args) throws IOException {
+
+
+
+        Display display = new Display();
+
+        display.Menu();
+
+
+
+    }
+
+
+    
+}
